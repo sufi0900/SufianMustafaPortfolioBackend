c8906d379a1d56c6b34a80d5b3ff799c799e8cff
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-08-21 16:33:28 +0500

    first commit

--- controllers/skill.js
+++ controllers/skill.js
@@ -1,5 +1,6 @@
 import SkillModal from "../models/skill.js";
 import mongoose from "mongoose";
+import cache from "memory-cache"; // Adjust the caching library as needed
 
 export const createSkill = async (req, res) => {
   const Skill = req.body;
@@ -11,28 +12,128 @@ export const createSkill = async (req, res) => {
 
   try {
     await newSkill.save();
+
+    // Update the cache with the newly created Skill
+    const prefix = "Skills_";
+    const keys = cache.keys();
+    keys.forEach((key) => {
+      if (key.startsWith(prefix)) {
+        cache.del(key);
+      }
+    });
+
     res.status(201).json(newSkill);
   } catch (error) {
     res.status(404).json({ message: "Something went wrong" });
   }
 };
 
+export const deleteSkill = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(404)
+        .json({ message: `No Skill exists with id: ${id}` });
+    }
+
+    // Remove the Skill from the database
+    await SkillModal.findByIdAndRemove(id);
+
+    // Update the corresponding cache entries for all Skill cards
+    const prefix = "Skills_";
+    const keys = cache.keys();
+    keys.forEach((key) => {
+      if (key.startsWith(prefix)) {
+        cache.del(key);
+      }
+    });
+
+    res.json({ message: "Skill deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ message: "Something went wrong" });
+  }
+};
+
+export const updateSkill = async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    toptext1,
+    toptext2,
+    description,
+    creator,
+    imageFile,
+    imageFile1,
+    imageFile2,
+    imageFile3,
+    link,
+  } = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(404)
+        .json({ message: `No Skill exists with id: ${id}` });
+    }
+
+    // Update the Skill in the database
+    const updatedSkill = {
+      creator,
+      title,
+      toptext1,
+      toptext2,
+      description,
+      link,
+      imageFile,
+      imageFile1,
+      imageFile2,
+      imageFile3,
+      _id: id,
+    };
+    await SkillModal.findByIdAndUpdate(id, updatedSkill, { new: true });
+
+    // Update the corresponding cache entries for all Skill cards
+    const prefix = "Skills_";
+    const keys = cache.keys();
+    keys.forEach((key) => {
+      if (key.startsWith(prefix)) {
+        cache.del(key);
+      }
+    });
+
+    res.json(updatedSkill);
+  } catch (error) {
+    res.status(404).json({ message: "Something went wrong" });
+  }
+};
 export const getSkills = async (req, res) => {
   const { page } = req.query;
   try {
-    // const Skills = await SkillModal.find();
-    // res.status(200).json(Skills);
-
-    const limit = 20;
+    const limit = 3;
     const startIndex = (Number(page) - 1) * limit;
-    const total = await SkillModal.countDocuments({});
-    const Skills = await SkillModal.find().limit(limit).skip(startIndex);
-    res.json({
+
+    // Check if data is present in cache for the specific page
+    const cachedData = cache.get(`Skills_page_${page}`);
+    if (cachedData) {
+      return res.json(cachedData);
+    }
+
+    // Query the database to fetch the Skills for the specific page and get the total count simultaneously
+    const [Skills, total] = await Promise.all([
+      SkillModal.find().limit(limit).skip(startIndex).lean(),
+      SkillModal.countDocuments({}),
+    ]);
+
+    // Update cache with the fetched data for the specific page
+    const cachedSkills = {
       data: Skills,
       currentPage: Number(page),
       totalSkills: total,
       numberOfPages: Math.ceil(total / limit),
-    });
+    };
+    cache.put(`Skills_page_${page}`, cachedSkills);
+
+    res.json(cachedSkills);
   } catch (error) {
     res.status(404).json({ message: "Something went wrong" });
   }
@@ -53,56 +154,16 @@ export const getSkillsByUser = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ message: "User doesn't exist" });
   }
+  const cacheKey = `Skills_user_${id}`;
+  const cachedData = cache.get(cacheKey);
+  if (cachedData) {
+    return res.json(cachedData);
+  }
+
   const userSkills = await SkillModal.find({ creator: id });
+
+  // Update cache with the fetched data for the specific user
+  cache.put(cacheKey, userSkills);
+
   res.status(200).json(userSkills);
-};
-
-export const deleteSkill = async (req, res) => {
-  const { id } = req.params;
-  try {
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ message: `No Skill exist with id: ${id}` });
-    }
-    await SkillModal.findByIdAndRemove(id);
-    res.json({ message: "Skill deleted successfully" });
-  } catch (error) {
-    res.status(404).json({ message: "Something went wrong" });
-  }
-};
-
-export const updateSkill = async (req, res) => {
-  const { id } = req.params;
-  const {
-    title,
-    description,
-    creator,
-    percentage,
-    imageFile,
-    imageFile1,
-    imageFile2,
-    imageFile3,
-    tags,
-  } = req.body;
-  try {
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ message: `No Skill exist with id: ${id}` });
-    }
-
-    const updatedSkill = {
-      creator,
-      title,
-      percentage,
-      description,
-      tags,
-      imageFile,
-      imageFile1,
-      imageFile2,
-      imageFile3,
-      _id: id,
-    };
-    await SkillModal.findByIdAndUpdate(id, updatedSkill, { new: true });
-    res.json(updatedSkill);
-  } catch (error) {
-    res.status(404).json({ message: "Something went wrong" });
-  }
 };
